54692a205d94ef85ebf06d0efee89c3712e9feec
KET <varlamova_e.t@mail.ru>
2022-09-02 10:58:44 +0300

    1a

---
+++ 1 task/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int result = 1;
+while(i < b)
+{
+    result = result*a;
+}
+Console.WriteLine(result);
